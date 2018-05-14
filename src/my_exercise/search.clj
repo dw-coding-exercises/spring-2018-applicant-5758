9c4d8eed021092da55798b6f27f65ed76d416aa7
Anonymous Applicant <anonymous@example.com>
2018-05-14 00:43:55 -0400

    Add OCD ID derivation from address

--- src/my_exercise/search.clj
+++ src/my_exercise/search.clj
@@ -1,5 +1,30 @@
 (ns my-exercise.search
-  (:require [hiccup.page :refer [html5]]))
+  (:require [hiccup.page :refer [html5]]
+            [clojure.string :as s]))
+
+(defn format-place
+  "Formats place name string to OCD ID style."
+  [place]
+  (s/lower-case (s/replace place #" " "_")))
+
+(defn format-state
+  "Formats state abbreviation to OCD ID style."
+  [state]
+  (s/lower-case state))
+
+(defn generate-ocd-ids
+  "Generates a vector of OCD IDs for given address inputs."
+  [city state]
+  (let [ocd-ids []
+        country-division "ocd-division/country:us"
+        state-division (str country-division "/state:" (format-state state))
+        place-division (str state-division "/place:" (format-place city))]
+    (cond-> ocd-ids
+      true (conj country-division)
+      (not (s/blank? state))
+      (conj state-division)
+      (not (s/blank? city))
+      (conj place-division))))
 
 (defn header [_]
   [:head
@@ -11,9 +36,10 @@
 
 (defn results
   "HTML body of the search results page."
-  [_]
+  [request]
   [:div {:class "results"}
-   [:h1 "Upcoming Elections"]])
+   [:h1 "Upcoming Elections"]
+   [:p (str (generate-ocd-ids (get-in request [:params :city]) (get-in request [:params :state])))]])
 
 (defn page [request]
   (html5
